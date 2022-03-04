ddebb7677bcc2a515ee99be428497eb7c607bc4c
LisetteAntigua <antigua223@gmail.com>
2022-03-04 15:55:42 -0500

    TableView Controller

---
+++ kapstone the squel/Tournament.swift
@@ -0,0 +1,16 @@
+//
+//  Tournament.swift
+//  kapstone the squel
+//
+//  Created by Lisette Antigua on 3/4/22.
+//
+
+import Foundation
+import UIKit
+
+struct Tournament
+{
+    var name:String
+    var description:String
+    var image:UIImage
+}
